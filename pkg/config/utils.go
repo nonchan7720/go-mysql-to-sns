12bd69889897006c8f86de7593834e621060a32d
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-10-10 13:42:29 +0000

    ネストしててもOK

--- pkg/config/utils.go
+++ pkg/config/utils.go
@@ -55,12 +55,12 @@ func replaceQuote(key string) string {
 }
 
 func (m mapper) find(key string) (interface{}, bool) {
-	v, ok := m[replaceQuote(key)]
-	return v, ok
+	keys := strings.Split(replaceQuote(key), ".")
+	return nestedMapLookup(m, keys...)
 }
 
 func (m mapper) findWithDefault(key string, default_ interface{}) interface{} {
-	if v, ok := m[replaceQuote(key)]; ok {
+	if v, ok := m.find(key); ok {
 		return v
 	}
 	if v, ok := default_.(string); ok {
@@ -143,4 +143,36 @@ func goTemplate(str string, mp map[string]interface{}) string {
 		return ""
 	}
 	return buf.String()
+}
+
+func nestedMapLookup(m map[string]interface{}, ks ...string) (interface{}, bool) {
+	var ok bool
+	var val interface{}
+
+	// 入力の検証
+	if len(ks) == 0 {
+		return nil, false
+	}
+	if ks[0] == "" {
+		return nil, false
+	}
+
+	// 最初のキーで値を取得する。
+	val, ok = m[ks[0]]
+	if !ok {
+		return nil, false
+	}
+
+	// 最後のキーの場合、値を返す。
+	if len(ks) == 1 {
+		return val, true
+	}
+
+	// 値がマップの場合、再帰的にネストされたマップを探索する。
+	if m, ok := val.(map[string]interface{}); ok {
+		return nestedMapLookup(m, ks[1:]...)
+	}
+
+	// 値がマップでない場合、エラーを返す。
+	return nil, false
 }
--- pkg/config/utils_test.go
+++ pkg/config/utils_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
@@ -12,10 +13,16 @@ func TestFastTemplate(t *testing.T) {
 		"int":   1,
 		"float": 1.11,
 		"key":   "a6ea368b-3ceb-4729-820b-22e85cace255",
+		"nested": map[string]interface{}{
+			"a": "a",
+			"b": map[string]interface{}{
+				"c": "d",
+			},
+		},
 	}
-	tpl := `{{int}}-{{float}}-{{replaceAll key "-" ""}}`
+	tpl := `{{int}}-{{float}}-{{replaceAll key "-" ""}}-{{nested.a}}-{{replaceAll nested.b.c "d" "e"}}`
 	v := fastTemplate(tpl, mp)
-	require.Equal(t, v, `1-1.11-a6ea368b3ceb4729820b22e85cace255`)
+	require.Equal(t, v, `1-1.11-a6ea368b3ceb4729820b22e85cace255-a-e`)
 }
 
 func TestGoTemplate(t *testing.T) {
@@ -23,10 +30,16 @@ func TestGoTemplate(t *testing.T) {
 		"int":   1,
 		"float": 1.11,
 		"key":   "a6ea368b-3ceb-4729-820b-22e85cace255",
+		"nested": map[string]interface{}{
+			"a": "a",
+			"b": map[string]interface{}{
+				"c": "d",
+			},
+		},
 	}
-	tpl := `{{.int}}-{{.float}}-{{replaceAll .key "-" ""}}`
+	tpl := `{{.int}}-{{.float}}-{{replaceAll .key "-" ""}}-{{.nested.a}}-{{replaceAll .nested.b.c "d" "e"}}`
 	v := goTemplate(tpl, mp)
-	require.Equal(t, v, "1-1.11-a6ea368b3ceb4729820b22e85cace255")
+	require.Equal(t, v, "1-1.11-a6ea368b3ceb4729820b22e85cace255-a-e")
 }
 
 func TestUUIDFunction(t *testing.T) {
@@ -35,5 +48,36 @@ func TestUUIDFunction(t *testing.T) {
 		actual := fn(tpl, nil)
 		_, err := uuid.Parse(actual)
 		require.NoError(t, err)
+	}
+}
+
+func TestNestedMapLookup(t *testing.T) {
+	m := map[string]interface{}{
+		"a": map[string]interface{}{
+			"b": 1,
+			"c": map[string]interface{}{
+				"d": "foo",
+			},
+		},
+	}
+
+	tests := []struct {
+		ks   []string
+		want interface{}
+		ok   bool
+	}{
+		{ks: []string{"a", "b"}, want: 1, ok: true},
+		{ks: []string{"a", "c", "d"}, want: "foo", ok: true},
+		{ks: []string{"a", "x", "y"}, want: nil, ok: false},
+		{ks: []string{"a", "c", "x"}, want: nil, ok: false},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%v", tt.ks), func(t *testing.T) {
+			val, ok := nestedMapLookup(m, tt.ks...)
+			if val != tt.want || ok != tt.ok {
+				t.Errorf("want %v, got %v", tt.want, val)
+			}
+		})
 	}
 }
