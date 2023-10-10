8e06e0afc751acfbda9fa206a0f8fae773f2965f
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-10-10 04:33:40 +0000

    fasttemplateと標準のtemplateで関数使える様にした

--- pkg/config/utils_test.go
+++ pkg/config/utils_test.go
@@ -3,15 +3,37 @@ package config
 import (
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
-func TestTemplate(t *testing.T) {
+func TestFastTemplate(t *testing.T) {
 	mp := map[string]interface{}{
 		"int":   1,
 		"float": 1.11,
+		"key":   "a6ea368b-3ceb-4729-820b-22e85cace255",
 	}
-	tpl := "{{int}}-{{float}}"
-	v := templateExecute(tpl, mp)
-	require.Equal(t, v, "1-1.11")
+	tpl := `{{int}}-{{float}}-{{replaceAll key "-" ""}}`
+	v := fastTemplate(tpl, mp)
+	require.Equal(t, v, `1-1.11-a6ea368b3ceb4729820b22e85cace255`)
+}
+
+func TestGoTemplate(t *testing.T) {
+	mp := map[string]interface{}{
+		"int":   1,
+		"float": 1.11,
+		"key":   "a6ea368b-3ceb-4729-820b-22e85cace255",
+	}
+	tpl := `{{.int}}-{{.float}}-{{replaceAll .key "-" ""}}`
+	v := goTemplate(tpl, mp)
+	require.Equal(t, v, "1-1.11-a6ea368b3ceb4729820b22e85cace255")
+}
+
+func TestUUIDFunction(t *testing.T) {
+	tpl := `{{newUUID}}`
+	for _, fn := range []func(string, map[string]interface{}) string{goTemplate, fastTemplate} {
+		actual := fn(tpl, nil)
+		_, err := uuid.Parse(actual)
+		require.NoError(t, err)
+	}
 }
