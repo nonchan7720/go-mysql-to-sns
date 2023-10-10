1ae7d32c57ceebdffde5a6a94fb352f7ec4ed5cb
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-10-10 04:34:06 +0000

    使用するテンプレートタイプを選択できる様に変えた

--- pkg/config/aws_sqs.go
+++ pkg/config/aws_sqs.go
@@ -3,6 +3,8 @@ package config
 import (
 	"errors"
 	"strings"
+
+	"github.com/creasty/defaults"
 )
 
 type SQS struct {
@@ -11,17 +13,18 @@ type SQS struct {
 }
 
 type Queue struct {
-	TableName              string `yaml:"tableName"`
-	QueueName              string `yaml:"queueName"`
-	QueueUrl               string `yaml:"queueUrl"`
-	MessageGroupIdTemplate string `yaml:"messageGroupIdTemplate"`
+	TableName              string       `yaml:"tableName"`
+	QueueName              string       `yaml:"queueName"`
+	QueueUrl               string       `yaml:"queueUrl"`
+	MessageGroupIdTemplate string       `yaml:"messageGroupIdTemplate"`
+	TemplateType           TemplateType `yaml:"templateType" default:"Fast"`
 }
 
 func (t *Queue) GetMessageGroupId(mp map[string]interface{}) *string {
 	if !t.IsFIFO() {
 		return nil
 	}
-	value := templateExecute(t.MessageGroupIdTemplate, mp)
+	value := templateExecute(t.TemplateType, t.MessageGroupIdTemplate, mp)
 	return &value
 }
 
@@ -32,6 +35,17 @@ func (t *Queue) IsFIFO() bool {
 func (t *Queue) Validation() error {
 	if t.IsFIFO() && t.MessageGroupIdTemplate == "" {
 		return errors.New("For FIFO topics, MessageGroupIdTemplate must be set.")
+	}
+	return nil
+}
+
+func (t *Queue) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	if err := defaults.Set(t); err != nil {
+		return err
+	}
+	type plain Queue
+	if err := unmarshal((*plain)(t)); err != nil {
+		return err
 	}
 	return nil
 }
