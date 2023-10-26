f475a225f6e5ed6fd712f784a6af160ffab1861a
nonchan <50233291+nonchan7720@users.noreply.github.com>
2023-10-27 02:34:23 +0900

    fix insecure ignore host key (#20)
    * fix insecure ignore host key
    
    * fix

--- pkg/config/ssh.go
+++ pkg/config/ssh.go
@@ -2,17 +2,33 @@ package config
 
 import (
 	"fmt"
-	"net"
 	"os"
+	"path/filepath"
 
+	"github.com/creasty/defaults"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/knownhosts"
 )
 
 type SSH struct {
-	PrivateKey string `yaml:"privateKey"`
-	Host       string `yaml:"host"`
-	Port       int    `yaml:"port"`
-	Username   string `yaml:"username"`
+	PrivateKey        string   `yaml:"privateKey"`
+	Host              string   `yaml:"host"`
+	Port              int      `yaml:"port"`
+	Username          string   `yaml:"username"`
+	HostKeyAlgorithms []string `yaml:"hostKeyAlgorithms" default:"[\"ssh-ed25519\"]"`
+	KnownHosts        string   `yaml:"knownHosts"`
+}
+
+func (s *SSH) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	if err := defaults.Set(s); err != nil {
+		return err
+	}
+	s.KnownHosts = filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts")
+	type plain SSH
+	if err := unmarshal((*plain)(s)); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (conf *SSH) Conn() (*ssh.Client, error) {
@@ -24,15 +40,18 @@ func (conf *SSH) Conn() (*ssh.Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	hostKeyCallbackFunc := func(hostname string, remote net.Addr, key ssh.PublicKey) error {
-		return nil
+	hostKeyCallback, err := knownhosts.New(conf.KnownHosts)
+	if err != nil {
+		return nil, err
 	}
+
 	sshConf := &ssh.ClientConfig{
 		User: conf.Username,
 		Auth: []ssh.AuthMethod{
 			ssh.PublicKeys(signer),
 		},
-		HostKeyCallback: hostKeyCallbackFunc,
+		HostKeyCallback:   hostKeyCallback,
+		HostKeyAlgorithms: conf.HostKeyAlgorithms,
 	}
 	return ssh.Dial("tcp", fmt.Sprintf("%s:%d", conf.Host, conf.Port), sshConf)
 }
