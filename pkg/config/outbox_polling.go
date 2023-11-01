4a74c79f59bf53e206f5d60b009f2651b0f827e2
nonchan7720 <nozomi_nishinohara@n-creativesystem.com>
2023-11-01 09:02:21 +0000

    producerNameを追加する

--- pkg/config/outbox_polling.go
+++ pkg/config/outbox_polling.go
@@ -1,32 +1,37 @@
 package config
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/creasty/defaults"
 )
 
-type OutboxConfig struct {
+type OutboxPollingConfig struct {
 	TransformTable  `yaml:",inline"`
+	ProducerName    string        `yaml:"producerName"`
 	MaxRetryCount   int           `yaml:"retryCount" default:"10"`
 	PollingInterval time.Duration `yaml:"pollingInterval" default:"5s"`
 	RetryBackOff    time.Duration `yaml:"retryBackoff" default:"20s"`
 }
 
-func (c *OutboxConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
+func (c *OutboxPollingConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	if err := defaults.Set(c); err != nil {
 		return err
 	}
-	type plain OutboxConfig
+	type plain OutboxPollingConfig
 	if err := unmarshal((*plain)(c)); err != nil {
 		return err
+	}
+	if c.ProducerName == "" {
+		return fmt.Errorf("producerName is required.")
 	}
 	return nil
 }
 
 type OutboxPolling struct {
 	Config       `yaml:",inline"`
-	OutboxConfig OutboxConfig `yaml:"outbox"`
+	OutboxConfig OutboxPollingConfig `yaml:"outbox"`
 }
 
 func LoadOutboxPollingConfig(filePath string) (*OutboxPolling, error) {
